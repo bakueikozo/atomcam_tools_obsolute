03c2f164acea71bcbd7840444fd0b18b2bba0ef6
Mitsuru Nakada <mitsuru_nakada@mac.com>
2021-11-17 21:38:54 +0900

    atomcam_toolsのverもwebUIに表示するよう修正

--- make.sh
+++ make.sh
@@ -14,7 +14,7 @@ cp -pr atomcam_configs/overlay_initramfs/* initramfs_skeleton
 docker-compose up -d
 docker-compose exec builder /src/atomcam_configs/scripts/build_all
 
-mv uImage.lzma ../factory_t31_ZMC6tiIDQN
-mv rootfs.ext2 ../rootfs_hack.ext2
+cp uImage.lzma ../factory_t31_ZMC6tiIDQN
+cp rootfs.ext2 ../rootfs_hack.ext2
 cd ..
 zip -ry atomcam_tools.zip factory_t31_ZMC6tiIDQN rootfs_hack.ext2 hostname authorized_keys
