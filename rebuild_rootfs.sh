9244b27851a0337b3f0e627d8a7a2fac7cc4ad09
Mitsuru Nakada <mitsuru_nakada@mac.com>
2021-12-16 21:51:09 +0900

    target directoryに生成ファイルを移動

--- rebuild_rootfs.sh
+++ rebuild_rootfs.sh
@@ -6,4 +6,5 @@ if ! docker ps | grep atomcam_tools_builder_1 ; then
   docker-compose up -d
 fi
 docker-compose exec builder /src/buildscripts/rebuild_rootfs
-zip -ry atomcam_tools.zip factory_t31_ZMC6tiIDQN rootfs_hack.ext2 hostname authorized_keys
+cd target
+zip -ry ../atomcam_tools.zip factory_t31_ZMC6tiIDQN rootfs_hack.ext2 hostname authorized_keys
