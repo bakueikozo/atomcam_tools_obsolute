058ece6b0af4a2577faf3d66774046eae0b98239
Mitsuru Nakada <mitsuru_nakada@mac.com>
2022-01-08 21:57:33 +0900

    curl hookの構造体を簡略化

--- libcallback/curl.c
+++ libcallback/curl.c
@@ -41,101 +41,23 @@ typedef int (*curl_write_callback)(char *buffer,
                                       int nitems,
                                       void *outstream);
 
-struct UserDefined {
-  FILE *err;
-  void *debugdata;
-  char *errorbuffer;
-  long proxyport;
-  void *out;
-  void *in_set;
-  void *writeheader;
-  void *rtp_out;
-  long use_port;
-  unsigned long httpauth;
-  unsigned long proxyauth;
-  unsigned long socks5auth;
-  long followlocation;
-  long maxredirs;
-  int keep_post;
-  void *postfields;
-  void *seek_func;
-  curl_off_t postfieldsize;
-  unsigned short localport;
-  int localportrange;
-  curl_write_callback fwrite_func;
-  void *fwrite_header;
-  void *fwrite_rtp;
-  void *fread_func_set;
-  void *fprogress;
-  void *fxferinfo;
-  void *fdebug;
-  void *ioctl_func;
-  void *fsockopt;
-  void *sockopt_client;
-  void *fopensocket;
-  void *opensocket_client;
-  void * fclosesocket;
-  void *closesocket_client;
-  void *seek_client;
-  void * convfromnetwork;
-  void * convtonetwork;
-  void * convfromutf8;
-  void *progress_client;
-  void *ioctl_client;
-  long timeout;
-  long connecttimeout;
-  long accepttimeout;
-  long happy_eyeballs_timeout;
-  long server_response_timeout;
-  long maxage_conn;
-  long tftp_blksize;
-  curl_off_t filesize;
-  long low_speed_limit;
-  long low_speed_time;
-  curl_off_t max_send_speed;
-  curl_off_t max_recv_speed;
-  curl_off_t set_resume_from;
-  void *headers;
-  void *proxyheaders;
-  void *httppost;
-  curl_off_t mimepost_pad;
-  unsigned char mimepost[352];
-  void *quote;
-  void *postquote;
-  void *prequote;
-  void *source_quote;
-  void *source_prequote;
-  void *source_postquote;
-  void *telnet_options;
-  void *resolve;
-  void *connect_to;
-  int timecondition;
-  time_t timevalue;
-  Curl_HttpReq httpreq;
-  long httpversion;
-};
-
-struct DynamicStatic {
-  char *url;
-  char *referer;
-  void *cookielist;
-  void *resolve;
-  unsigned int bit;
-};
-
-struct PureInfo {
-  int httpcode;
-};
-
 struct SessionHandle {
-  unsigned char padding[1392];
-  union {
-    unsigned char set_bin[1328];
-    struct UserDefined set;
-  } ud;
-  struct DynamicStatic change;
-  unsigned char padding2[988];
-  struct PureInfo info;
+  unsigned char padding0[1392];
+  unsigned char padding1[16];
+  void *out; // offset 1392 + 16
+  unsigned char padding2[40];
+  void *postfields; // offset 1392 + 60
+  unsigned char padding3[8];
+  curl_off_t postfieldsize; // offset offset 1392 + 72
+  unsigned char padding4[8];
+  curl_write_callback fwrite_func; // offset 1392 + 88
+  unsigned char padding5[568];
+  Curl_HttpReq httpreq; // offset 1392 + 660
+  unsigned char padding6[664];
+  char *url; // offset 2720 + 0
+  unsigned char padding7[16];
+  unsigned char padding8[988];
+  int httpcode; // offset 3728 + 0
 };
 
 static CURLcode (*original_curl_easy_perform)(CURL *curl);
@@ -175,25 +97,25 @@ CURLcode curl_easy_perform(struct SessionHandle *data) {
     : "=r"(ra)
   );
 
-  int method = data->ud.set.httpreq;
+  int method = data->httpreq;
   if(method > HTTPREQ_LAST) method = HTTPREQ_LAST;
-  printf("[curl-debug] %s %s ra=0x%08x\n", methods[method], data->change.url, ra);
-  if(data->ud.set.postfields) {
-    if(data->ud.set.postfieldsize > 0) {
-      Dump("[curl-debug] post", data->ud.set.postfields, data->ud.set.postfieldsize);
+  printf("[curl-debug] %s %s ra=0x%08x\n", methods[method], data->url, ra);
+  if(data->postfields) {
+    if(data->postfieldsize > 0) {
+      Dump("[curl-debug] post", data->postfields, data->postfieldsize);
     } else {
-      printf("[curl-debug] post : %s\n", data->ud.set.postfields);
+      printf("[curl-debug] post : %s\n", data->postfields);
     }
   }
 
-  if(data->change.url && !strcmp(data->change.url + strlen(data->change.url) - strlen(AlarmPath), AlarmPath)) {
+  if(data->url && !strcmp(data->url + strlen(data->url) - strlen(AlarmPath), AlarmPath)) {
     static time_t lastAccess = 0;
     struct timeval now;
     gettimeofday(&now, NULL);
     if(now.tv_sec - lastAccess < 300) {
       printf("[curl-debug] Dismiss short cycle alarms.\n");
-      memcpy(data->ud.set.out, DummyRes, strlen(DummyRes));
-      data->info.httpcode = 200;
+      memcpy(data->out, DummyRes, strlen(DummyRes));
+      data->httpcode = 200;
       return CURLE_OK;
     }
     CURLcode res = original_curl_easy_perform(data);
@@ -202,14 +124,14 @@ CURLcode curl_easy_perform(struct SessionHandle *data) {
     return res;
   }
 
-  if(data->change.url && !strncmp(data->change.url, DummyHost, strlen(DummyHost))) {
+  if(data->url && !strncmp(data->url, DummyHost, strlen(DummyHost))) {
     printf("[curl-debug] skip http-post.\n");
-    data->info.httpcode = 200;
+    data->httpcode = 200;
     return CURLE_OK;
   }
 
   CURLcode res = original_curl_easy_perform(data);
-  if(data->ud.set.out) printf("[curl-debug] res : %s\n", data->ud.set.out);
+  if(data->out) printf("[curl-debug] res : %s\n", data->out);
   printf("[curl-debug] ret: %d\n", res);
   return res;
 }
