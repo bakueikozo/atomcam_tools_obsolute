372f19d6c674c4996d9e2ed138b78bc8c755f2c9
Mitsuru Nakada <mitsuru_nakada@mac.com>
2022-01-08 16:12:32 +0900

    build scriptの多重メンテ対策

--- make.sh
+++ make.sh
@@ -6,6 +6,6 @@ if docker ps | grep atomcam_tools_builder_1 ; then
   docker stop atomcam_tools_builder_1
 fi
 docker-compose up -d
-docker-compose exec builder /src/buildscripts/build_all
+docker-compose exec builder /src/buildscripts/build_all new
 cd target
 zip -ry ../atomcam_tools.zip factory_t31_ZMC6tiIDQN rootfs_hack.ext2 hostname authorized_keys
--- rebuild_rootfs.sh
+++ rebuild_rootfs.sh
@@ -5,6 +5,6 @@ set -x
 if ! docker ps | grep atomcam_tools_builder_1 ; then
   docker-compose up -d
 fi
-docker-compose exec builder /src/buildscripts/rebuild_rootfs
+docker-compose exec builder /src/buildscripts/build_all rebuild
 cd target
 zip -ry ../atomcam_tools.zip factory_t31_ZMC6tiIDQN rootfs_hack.ext2 hostname authorized_keys
