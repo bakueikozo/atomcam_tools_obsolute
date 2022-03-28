5f75d7d1db0ca54c853d0c6084cbb63879dbde71
Mitsuru Nakada <mitsuru_nakada@mac.com>
2022-03-28 22:09:58 +0900

    build時にエラーが出たらzipイメージを作らずに停止する

--- make.sh
+++ make.sh
@@ -7,5 +7,6 @@ if docker ps | grep atomcam_tools_builder_1 ; then
 fi
 docker-compose up -d
 docker-compose exec builder /src/buildscripts/build_all new
+[ $? != 0 ] && exit $?
 cd target
 zip -ry ../atomcam_tools.zip factory_t31_ZMC6tiIDQN rootfs_hack.ext2 hostname authorized_keys
--- rebuild_rootfs.sh
+++ rebuild_rootfs.sh
@@ -6,5 +6,6 @@ if ! docker ps | grep atomcam_tools_builder_1 ; then
   docker-compose up -d
 fi
 docker-compose exec builder /src/buildscripts/build_all rebuild
+[ $? != 0 ] && exit $?
 cd target
 zip -ry ../atomcam_tools.zip factory_t31_ZMC6tiIDQN rootfs_hack.ext2 hostname authorized_keys
