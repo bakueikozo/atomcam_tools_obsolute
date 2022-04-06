023b844e67a80b4c93334062a128e4e57b5fb112
Roger Leung <rleung@datawords.com>
2022-04-06 11:08:28 +0800

    use network only when there is no wifi error

--- configs/overlay_rootfs/scripts/watchdog.sh
+++ configs/overlay_rootfs/scripts/watchdog.sh
@@ -7,28 +7,30 @@ router=`ip route | awk '/default/ { print $3 }'`
 count=0
 wifi_error=0
 while sleep 20 ; do
+  let count++
+
   # wifi
   if ping -c 1 $router > /dev/null ; then
     wifi_error=0
+
+    # process check 1min interval
+      if [ `expr $count % 3` -eq 0 ]; then
+        /scripts/lighttpd.sh watchdog >> /media/mmc/atomhack.log
+        /scripts/rtspserver.sh watchdog >> /media/mmc/atomhack.log
+
+        HEALTHCHECK=$(awk -F "=" '/HEALTHCHECK *=/ {print $2}' $HACK_INI)
+        HEALTHCHECK_PING_URL=$(awk -F "=" '/HEALTHCHECK_PING_URL *=/ {print $2}' $HACK_INI)
+        [ "$HEALTHCHECK" == "on" ] && [ "$HEALTHCHECK_PING_URL" != "" ] && echo $(date) >> /media/mmc/healthcheck.log && curl -fsS -m 10 --retry 5 -o /dev/null $HEALTHCHECK_PING_URL
+      fi
   else
     let wifi_error++
   fi
+
   if [ $wifi_error -ge 3 ]; then
     echo "WiFi restart" >> /media/mmc/atomhack.log
     ifconfig wlan0 down
     ifconfig wlan0 up
     wifi_error=0
-  fi
-
-  let count++
-
-  # process check 1min interval
-  if [ `expr $count % 3` -eq 0 ]; then
-    /scripts/lighttpd.sh watchdog >> /media/mmc/atomhack.log
-    /scripts/rtspserver.sh watchdog >> /media/mmc/atomhack.log
-    HEALTHCHECK=$(awk -F "=" '/HEALTHCHECK *=/ {print $2}' $HACK_INI)
-    HEALTHCHECK_PING_URL=$(awk -F "=" '/HEALTHCHECK_PING_URL *=/ {print $2}' $HACK_INI)
-    [ "$HEALTHCHECK" == "on" ] && [ "$HEALTHCHECK_PING_URL" != "" ] && curl -fsS -m 10 --retry 5 -o /tmp/log/healthcheck.log $HEALTHCHECK_PING_URL
   fi
 
   # free memory
@@ -38,6 +40,3 @@ while sleep 20 ; do
     free >> /media/mmc/atomhack.log
   fi
 done
-
-
-
