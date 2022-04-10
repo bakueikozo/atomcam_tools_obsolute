543965db846c04eca105904f9f7c4a88abe337dc
y2kbug <y2kbug@thematrix.dev>
2022-04-10 23:10:02 +0800

    try fixing connection related problems

--- configs/overlay_rootfs/scripts/watchdog.sh
+++ configs/overlay_rootfs/scripts/watchdog.sh
@@ -20,7 +20,7 @@ while sleep 20 ; do
 
       HEALTHCHECK=$(awk -F "=" '/HEALTHCHECK *=/ {print $2}' $HACK_INI)
       HEALTHCHECK_PING_URL=$(awk -F "=" '/HEALTHCHECK_PING_URL *=/ {print $2}' $HACK_INI)
-      [ "$HEALTHCHECK" == "on" ] && [ "$HEALTHCHECK_PING_URL" != "" ] && echo $(TZ=JST-9 date +"%Y/%m/%d %H:%M:%S : ") `curl -fsS -m 10 --retry 5 $HEALTHCHECK_PING_URL` >> /media/mmc/healthcheck.log
+      [ "$HEALTHCHECK" == "on" ] && [ "$HEALTHCHECK_PING_URL" != "" ] && echo $(TZ=JST-9 date +"%Y/%m/%d %H:%M:%S : ") `curl --ipv4 --location --silent --show-error --output /dev/null --write-out "%{http_code}" $HEALTHCHECK_PING_URL` >> /media/mmc/healthcheck.log
     fi
   else
     let wifi_error++
