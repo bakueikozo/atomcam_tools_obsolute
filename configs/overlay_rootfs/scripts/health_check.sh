b62a2e473470db2cfd64e9ec7afabcebcbe98743
y2kbug <y2kbug@thematrix.dev>
2022-04-12 09:40:39 +0800

    set curl timeout and retries to avoid false alarm

--- configs/overlay_rootfs/scripts/health_check.sh
+++ configs/overlay_rootfs/scripts/health_check.sh
@@ -2,4 +2,4 @@
 
 HACK_INI=/tmp/hack.ini
 HEALTHCHECK_PING_URL=$(awk -F "=" '/HEALTHCHECK_PING_URL *=/ {print $2}' $HACK_INI)
-[ "$HEALTHCHECK_PING_URL" != "" ] && echo $(TZ=JST-9 date +"%Y/%m/%d %H:%M:%S : ") `curl --ipv4 --location --silent --show-error --output /dev/null --write-out "%{http_code}" $HEALTHCHECK_PING_URL` >> /media/mmc/healthcheck.log
+[ "$HEALTHCHECK_PING_URL" != "" ] && echo $(TZ=JST-9 date +"%Y/%m/%d %H:%M:%S : ") `curl --ipv4 --max-time 10 --retry 5 --location --silent --show-error --output /dev/null --write-out "%{http_code}" $HEALTHCHECK_PING_URL` >> /media/mmc/healthcheck.log
