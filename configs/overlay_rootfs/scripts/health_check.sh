46f51f4aafa802bba10b817fc51b3cf62f193ec5
Mitsuru Nakada <nakada@geckolink.com>
2022-05-02 16:49:49 +0900

    bug-fix: - networkが繋がらない状態の時にudhcpcが終わらない 機能追加: - retryを繰り返しても接続できない時に再起動する - 再起動時にhealthcheck.logに状態の出力を追加

--- configs/overlay_rootfs/scripts/health_check.sh
+++ configs/overlay_rootfs/scripts/health_check.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-[ ! -f /tmp/router_address ] && exit 0
 [ -f /tmp/healthcheck.lock ] && exit 0
 touch /tmp/healthcheck.lock
 
 RES=1
-ROUTER=`cat /tmp/router_address`
+[ -f /tmp/router_address ] && ROUTER=`cat /tmp/router_address`
 if [ "$ROUTER" = "" ]; then
   ip route | awk '/default/ { print $3 }' > /tmp/router_address
   ROUTER=`cat /tmp/router_address`
@@ -23,19 +22,36 @@ if [ $RES -eq 1 ] ; then
 fi
 
 if [ $RES -eq 0 ] ; then
-  retry=0
-  [ -f /tmp/healthcheck.retry_count ] && retry=`cat /tmp/healthcheck.retry_count`
+  retry_count="0 0"
+  [ -f /tmp/healthcheck.retry_count ] && retry_count=`cat /tmp/healthcheck.retry_count`
+  retry=${retry_count% *}
+  error=${retry_count#* }
   let retry++
   echo $(date +"%Y/%m/%d %H:%M:%S : retry : ") $retry >> /media/mmc/healthcheck.log
   if [ $retry -ge 3 ] ; then
-    echo $(date +"%Y/%m/%d %H:%M:%S : WiFi restart") >> /media/mmc/healthcheck.log
+    retry=0
+    let error++
+    if [ $error -ge 10 ] ; then
+      echo $(date +"%Y/%m/%d %H:%M:%S : retry error -> reboot") >> /media/mmc/healthcheck.log
+      echo router=$ROUTER >> /media/mmc/healthcheck.log
+      [ "$ROUTER" != "" ] && ping -c 1 $ROUTER >> /media/mmc/healthcheck.log 2>&1
+      ping -c 1 8.8.8.8 >> /media/mmc/healthcheck.log 2>&1
+      ifconfig wlan0 >> /media/mmc/healthcheck.log 2>&1
+      df -k >> /media/mmc/healthcheck.log
+      free >> /media/mmc/healthcheck.log
+      killall -SIGUSR2 iCamera_app
+      sync
+      sync
+      sync
+      reboot
+    fi
+    echo $(date +"%Y/%m/%d %H:%M:%S : WiFi restart : error : ") $error >> /media/mmc/healthcheck.log
     ifconfig wlan0 down
     ifconfig wlan0 up
-    killall -USR1 udhcpc || udhcpc -i wlan0 -p /var/run/udhcpc.pid
-    rm -f /tmp/healthcheck.retry_count
-  else
-    echo $retry > /tmp/healthcheck.retry_count
+    killall -USR1 udhcpc || udhcpc -i wlan0 -n -p /var/run/udhcpc.pid >> /media/mmc/healthcheck.log 2>&1
+
   fi
+  echo $retry $error > /tmp/healthcheck.retry_count
 else
   rm -f /tmp/healthcheck.retry_count
 fi
