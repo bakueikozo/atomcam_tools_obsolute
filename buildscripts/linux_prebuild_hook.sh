54a69852cd6ded76db9185f55e74195e97a85deb
Mitsuru Nakada <mitsuru_nakada@mac.com>
2022-06-13 22:04:48 +0900

    機能追加: - v4l2rtspstreamer defasult formatにHEVC追加

--- buildscripts/linux_prebuild_hook.sh
+++ buildscripts/linux_prebuild_hook.sh
@@ -3,8 +3,3 @@
 # Create the cpio root filesystem that is embedded in the kernel
 # This is a minimal root filesystem to bootstrap the bigger rootfs
 /src/buildscripts/make_initramfs.sh /atomtools/build/buildroot-2016.02/output
-
-# package patch
-for i in `ls /src/patches/package` ; do
-  cp -pr /src/patches/package/$i/*patch /atomtools/build/buildroot-2016.02/package/$i
-done
