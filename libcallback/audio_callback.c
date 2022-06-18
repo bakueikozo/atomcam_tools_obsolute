2d008a8fc3bc386be403b706fdb5f4db39dac7b5
Mitsuru Nakada <mitsuru_nakada@mac.com>
2022-06-18 14:57:08 +0900

    機能追加: - /scripts/cmd videoに下記サブコマンド追加   - flip [normal/flip/mirror/flip_mirror]   -  cont 0 - 255(center:128)   -  bri 0 - 255(center:128)   -  sat 0 - 255(center:128)   -  sharp 0 - 255(center:128)   - sinter 0 - 255(center:128)   -  temper 0 - 255(center:128)   -  aecomp 0 - 255   -  aeitmax 0-   -  dpc 0 - 255   -  drc 0 - 255   - / hilight 0 - 10   -  again 0 -   -  dgain 0 - - /scripts/cmd audioに下記サブコマンド追加  -  hpf on/off  -   agc gainLevel:0-31(dB) maxGain:0-90(dB)  -   ns off/0-3  -   aec on/off  -   vol -30 - 120  -   gain 0 - 31  -   alc 0-7

--- libcallback/audio_callback.c
+++ libcallback/audio_callback.c
@@ -10,6 +10,50 @@
 #include <sys/time.h>
 #include <tinyalsa/pcm.h>
 
+static const int AudioDecviceID = 1;
+static const int AudioChID = 0;
+
+typedef struct {
+  int samplerate; // Audio sampling rate. 8000 Hz
+  int bitwidth; // Audio sampling precision. 16 bit
+  int soundmode; // Audio channel mode. 1(mono)
+  int frmNum;  // Number of cached frames, range: [2, MAX_AUDIO_FRAME_NUM].
+  int numPerFrm; // Number of sample points per frame.
+  int chnCnt;  // Number of channels supported.
+} IMPAudioIOAttr;
+
+// attribute of the audio input device.
+extern int IMP_AI_SetPubAttr(int audioDevId, IMPAudioIOAttr *attr);
+extern int IMP_AI_GetPubAttr(int audioDevId, IMPAudioIOAttr *attr);
+
+// AI high pass filtering function.
+extern int IMP_AI_DisableHpf();
+extern int IMP_AI_EnableHpf(IMPAudioIOAttr *attr);
+
+// AI automatic gain feature.
+extern int IMP_AI_DisableAgc();
+extern int IMP_AI_EnableAgc(IMPAudioIOAttr *attr, short TargetLevelDbfs, short CompressionGaindB);
+
+// Noise suppression.
+extern int IMP_AI_DisableNs();
+extern int IMP_AI_EnableNs(IMPAudioIOAttr *attr, int mode);
+
+// Enable audio echo cancellation feature of the specified audio input and audio output.
+extern int IMP_AI_DisableAec(int aiDevId, int aiCh);
+extern int IMP_AI_EnableAec(int aiDevId, int aiChn, int aoDevId, int aoChn);
+
+// audio input volume. -30 - 120, default: 60
+extern int IMP_AI_SetVol(int audioDevId, int aiChn, int aiVol);
+extern int IMP_AI_GetVol(int audioDevId, int aiChn, int *vol);
+
+// audio input gain. 0 - 31
+extern int IMP_AI_SetGain(int audioDevId, int aiChn, int aiGain);
+extern int IMP_AI_GetGain(int audioDevId, int aiChn, int *aiGain);
+
+//alc gain value. 0 - 7
+extern int IMP_AI_SetAlcGain(int audioDevId, int aiChn, int aiPgaGain);
+extern int IMP_AI_GetAlcGain(int audioDevId, int aiChn, int *aiPgaGain);
+
 struct frames_st {
   void *buf;
   size_t length;
@@ -34,9 +78,140 @@ struct audio_capture_st audio_capture[] = {
   },
 };
 
+static char audioResBuf[256];
+
+static char *HighPassFilter(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  int ret = -1;
+  if(p && !strcmp(p, "off")) {
+    ret = IMP_AI_DisableHpf();
+  }
+  if(p && !strcmp(p, "on")) {
+    IMPAudioIOAttr attr;
+    ret = IMP_AI_GetPubAttr(AudioDecviceID, &attr);
+    if(!ret) ret = IMP_AI_EnableHpf(&attr);
+  }
+  return ret ? "error" : "ok";
+}
+
+static char *AutoGainControl(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) return "error";
+
+  int ret = -1;
+  if(!strcmp(p, "off")) {
+    // ret = IMP_AI_DisableAgc(); // Exception
+  } else {
+    int targetLevelDbfs = atoi(p);
+    p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+    if(p) {
+      int compressionGaindB = atoi(p);
+      IMPAudioIOAttr attr;
+      ret = IMP_AI_GetPubAttr(AudioDecviceID, &attr);
+      if(!ret) ret = IMP_AI_EnableAgc(&attr, targetLevelDbfs, compressionGaindB);
+    }
+  }
+  return ret ? "error" : "ok";
+}
+
+static char *NoiseSuppression(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) return "error";
+
+  int ret = -1;
+  if(!strcmp(p, "off")) {
+    ret = IMP_AI_DisableNs();
+  } else {
+    int level = atoi(p);
+    IMPAudioIOAttr attr;
+    ret = IMP_AI_GetPubAttr(AudioDecviceID, &attr);
+    if(!ret) ret = IMP_AI_EnableNs(&attr, level);
+  }
+  return ret ? "error" : "ok";
+}
+
+static char *EchoCancellation(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  int ret = -1;
+  if(p && !strcmp(p, "off")) {
+    ret = IMP_AI_DisableAec(AudioDecviceID, AudioChID);
+  }
+  if(p && !strcmp(p, "on")) {
+    ret = IMP_AI_EnableAec(AudioDecviceID, AudioChID, AudioDecviceID, AudioChID);
+  }
+  return ret ? "error" : "ok";
+}
+
+static char *Volume(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    int vol;
+    int ret = IMP_AI_GetVol(AudioDecviceID, AudioChID, &vol);
+    if(ret) return "error";
+    sprintf(audioResBuf, "%d\n", vol);
+    return audioResBuf;
+  }
+  int ret = IMP_AI_SetVol(AudioDecviceID, AudioChID, atoi(p));
+  return ret ? "error" : "ok";
+}
+
+static char *Gain(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    int gain;
+    int ret = IMP_AI_GetGain(AudioDecviceID, AudioChID, &gain);
+    if(ret) return "error";
+    sprintf(audioResBuf, "%d\n", gain);
+    return audioResBuf;
+  }
+  int ret = IMP_AI_SetGain(AudioDecviceID, AudioChID, atoi(p));
+  return ret ? "error" : "ok";
+}
+
+static char *AlcGain(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    int gain;
+    int ret = IMP_AI_GetAlcGain(AudioDecviceID, AudioChID, &gain);
+    if(ret) return "error";
+    sprintf(audioResBuf, "%d\n", gain);
+    return audioResBuf;
+  }
+  int ret = IMP_AI_SetAlcGain(AudioDecviceID, AudioChID, atoi(p));
+  return ret ? "error" : "ok";
+}
+
+struct CommandTableSt {
+  const char *cmd;
+  char * (*func)(char *);
+};
+
+static struct CommandTableSt AudioCommandTable[] = {
+  { "hpf",      &HighPassFilter }, // hpf on/off
+  { "agc",      &AutoGainControl }, // agc gainLevel:0-31(dB) maxGain:0-90(dB)
+  { "ns",       &NoiseSuppression }, // ns off/0-3
+  { "aec",      &EchoCancellation }, // aec on/off
+  { "vol",      &Volume }, // vol -30 - 120
+  { "gain",     &Gain }, // gain 0 - 31
+  { "alc",      &AlcGain }, // alc 0-7
+};
+
 char *AudioCapture(int fd, char *tokenPtr) {
 
   char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(p) {
+    for(int i = 0; i < sizeof(AudioCommandTable) / sizeof(struct CommandTableSt); i++) {
+      if(!strcmp(p, AudioCommandTable[i].cmd)) return (*AudioCommandTable[i].func)(tokenPtr);
+    }
+  }
+
   int ch = 0;
   if(p && (!strcmp(p, "0") || !strcmp(p, "1"))) {
     ch = atoi(p);
--- libcallback/video_callback.c
+++ libcallback/video_callback.c
@@ -11,6 +11,65 @@
 #include <sys/time.h>
 #include <pthread.h>
 
+// contrast of image effect.
+extern int IMP_ISP_Tuning_SetContrast(unsigned char contrast);
+extern int IMP_ISP_Tuning_GetContrast(unsigned char *pcontrast);
+
+// brightness of image effect.
+extern int IMP_ISP_Tuning_SetBrightness(unsigned char bright);
+extern int IMP_ISP_Tuning_GetBrightness(unsigned char *pbright);
+
+// saturation of image effect.
+extern int IMP_ISP_Tuning_SetSaturation(unsigned char sat);
+extern int IMP_ISP_Tuning_GetSaturation(unsigned char *psat);
+
+// sharpness of image effect.
+extern int IMP_ISP_Tuning_SetSharpness(unsigned char sharpness);
+extern int IMP_ISP_Tuning_GetSharpness(unsigned char *psharpness);
+
+// AE compensation.
+// AE compensation parameters can adjust the target of the image AE.
+extern int IMP_ISP_Tuning_SetAeComp(int comp);
+extern int IMP_ISP_Tuning_GetAeComp(int *comp);
+
+// AE Max parameters.
+extern int IMP_ISP_Tuning_SetAe_IT_MAX(unsigned int it_max);
+extern int IMP_ISP_Tuning_GetAE_IT_MAX(unsigned int *it_max);
+
+// DPC Strength.
+extern int IMP_ISP_Tuning_SetDPC_Strength(unsigned int ratio);
+extern int IMP_ISP_Tuning_GetDPC_Strength(unsigned int *ratio);
+
+// DRC Strength.
+extern int IMP_ISP_Tuning_SetDRC_Strength(unsigned int ratio);
+extern int IMP_ISP_Tuning_GetDRC_Strength(unsigned int *ratio);
+
+// highlight intensity controls.
+extern int IMP_ISP_Tuning_SetHiLightDepress(uint32_t strength);
+extern int IMP_ISP_Tuning_GetHiLightDepress(uint32_t *strength);
+
+// Set 3D noise reduction intensity.
+extern int IMP_ISP_Tuning_SetTemperStrength(uint32_t ratio);
+
+// Set 2D noise reduction intensity.
+extern int IMP_ISP_Tuning_SetSinterStrength(uint32_t ratio);
+
+// Max value of sensor analog gain.
+extern int IMP_ISP_Tuning_SetMaxAgain(uint32_t gain);
+extern int IMP_ISP_Tuning_GetMaxAgain(uint32_t *gain);
+
+// Max value of sensor Digital gain.
+extern int IMP_ISP_Tuning_SetMaxDgain(uint32_t gain);
+extern int IMP_ISP_Tuning_GetMaxDgain(uint32_t *gain);
+
+// ISP image mirror(horizontal) effect function (enable/disable)
+extern int IMP_ISP_Tuning_SetISPHflip(int mode);
+extern int IMP_ISP_Tuning_GetISPHflip(int *pmode);
+
+// ISP image mirror(vertical) effect function (enable/disable)
+extern int IMP_ISP_Tuning_SetISPVflip(int mode);
+extern int IMP_ISP_Tuning_GetISPVflip(int *pmode);
+
 struct frames_st {
   unsigned char *buf;
   size_t length;
@@ -32,6 +91,7 @@ struct video_capture_st {
   int initialized;
   int fd;
 };
+
 static struct video_capture_st video_capture[] = {
   {
     .capture = video0_encode_capture,
@@ -59,9 +119,229 @@ static struct video_capture_st video_capture[] = {
   },
 };
 
+static char videoResBuf[256];
+
+static char *Flip(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    int vflip, hflip;
+    IMP_ISP_Tuning_GetISPHflip(&hflip);
+    IMP_ISP_Tuning_GetISPVflip(&vflip);
+    if(!hflip && !vflip) return "normal";
+    if(hflip && !vflip) return "flip";
+    if(!hflip && vflip) return "mirror";
+    return "flip_mirror";
+  }
+
+  if(!strcmp(p, "normal")) {
+    IMP_ISP_Tuning_SetISPVflip(0);
+    IMP_ISP_Tuning_SetISPHflip(0);
+  } else if(!strcmp(p, "flip")) {
+    IMP_ISP_Tuning_SetISPVflip(1);
+    IMP_ISP_Tuning_SetISPHflip(0);
+  } else if(!strcmp(p, "mirror")) {
+    IMP_ISP_Tuning_SetISPVflip(0);
+    IMP_ISP_Tuning_SetISPHflip(1);
+  } else if(!strcmp(p, "flip_mirror")) {
+    IMP_ISP_Tuning_SetISPVflip(1);
+    IMP_ISP_Tuning_SetISPHflip(1);
+  } else {
+    return "error";
+  }
+  return "ok";
+}
+
+static char *Contrast(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    unsigned char cont;
+    IMP_ISP_Tuning_GetContrast(&cont);
+    sprintf(videoResBuf, "%d\n", cont);
+    return videoResBuf;
+  }
+  int res = IMP_ISP_Tuning_SetContrast(atoi(p));
+  return res ? "error": "ok";
+}
+
+static char *Brightness(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    unsigned char bri;
+    IMP_ISP_Tuning_GetBrightness(&bri);
+    sprintf(videoResBuf, "%d\n", bri);
+    return videoResBuf;
+  }
+  int res = IMP_ISP_Tuning_SetBrightness(atoi(p));
+  return res ? "error": "ok";
+}
+
+static char *Saturation(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    unsigned char sat;
+    IMP_ISP_Tuning_GetSaturation(&sat);
+    sprintf(videoResBuf, "%d\n", sat);
+    return videoResBuf;
+  }
+  int res = IMP_ISP_Tuning_SetSaturation(atoi(p));
+  return res ? "error": "ok";
+}
+
+static char *Sharpness(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    unsigned char sharpness;
+    IMP_ISP_Tuning_GetSharpness(&sharpness);
+    sprintf(videoResBuf, "%d\n", sharpness);
+    return videoResBuf;
+  }
+  int res = IMP_ISP_Tuning_SetSharpness(atoi(p));
+  return res ? "error": "ok";
+}
+
+static char *AEComp(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    int comp;
+    IMP_ISP_Tuning_GetAeComp(&comp);
+    sprintf(videoResBuf, "%d\n", comp);
+    return videoResBuf;
+  }
+  int res = IMP_ISP_Tuning_SetAeComp(atoi(p));
+  return res ? "error": "ok";
+}
+
+static char *AEItMax(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    unsigned int itMax;
+    IMP_ISP_Tuning_GetAE_IT_MAX(&itMax);
+    sprintf(videoResBuf, "%d\n", itMax);
+    return videoResBuf;
+  }
+  int res = IMP_ISP_Tuning_SetAe_IT_MAX(atoi(p));
+  return res ? "error": "ok";
+}
+
+static char *Sinter(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) return "error";
+  int res = IMP_ISP_Tuning_SetSinterStrength(atoi(p));
+  return res ? "error": "ok";
+}
+
+static char *Temper(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) return "error";
+  int res = IMP_ISP_Tuning_SetTemperStrength(atoi(p));
+  return res ? "error": "ok";
+}
+
+static char *DPC(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    unsigned int dpc;
+    IMP_ISP_Tuning_GetDPC_Strength(&dpc);
+    sprintf(videoResBuf, "%d\n", dpc);
+    return videoResBuf;
+  }
+  int res = IMP_ISP_Tuning_SetDPC_Strength(atoi(p));
+  return res ? "error": "ok";
+}
+
+static char *DRC(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    unsigned int drc;
+    IMP_ISP_Tuning_GetDRC_Strength(&drc);
+    sprintf(videoResBuf, "%d\n", drc);
+    return videoResBuf;
+  }
+  int res = IMP_ISP_Tuning_SetDRC_Strength(atoi(p));
+  return res ? "error": "ok";
+}
+
+static char *HiLight(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    unsigned int strength;
+    IMP_ISP_Tuning_GetHiLightDepress(&strength);
+    sprintf(videoResBuf, "%d\n", strength);
+    return videoResBuf;
+  }
+  int res = IMP_ISP_Tuning_SetHiLightDepress(atoi(p));
+  return res ? "error": "ok";
+}
+
+static char *AGain(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    unsigned int gain;
+    IMP_ISP_Tuning_GetMaxAgain(&gain);
+    sprintf(videoResBuf, "%d\n", gain);
+    return videoResBuf;
+  }
+  int res = IMP_ISP_Tuning_SetMaxAgain(atoi(p));
+  return res ? "error": "ok";
+}
+
+static char *DGain(char *tokenPtr) {
+
+  char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(!p) {
+    unsigned int gain;
+    IMP_ISP_Tuning_GetMaxDgain(&gain);
+    sprintf(videoResBuf, "%d\n", gain);
+    return videoResBuf;
+  }
+  int res = IMP_ISP_Tuning_SetMaxDgain(atoi(p));
+  return res ? "error": "ok";
+}
+
+struct CommandTableSt {
+  const char *cmd;
+  char * (*func)(char *);
+};
+
+static struct CommandTableSt VideoCommandTable[] = {
+  { "flip",      &Flip }, // flip [normal/flip/mirror/flip_mirror]
+  { "cont",      &Contrast }, // cont 0 - 255(center:128)
+  { "bri",       &Brightness }, // bri 0 - 255(center:128)
+  { "sat",       &Saturation }, // sat 0 - 255(center:128)
+  { "sharp",     &Sharpness }, // sharp 0 - 255(center:128)
+  { "sinter",    &Sinter }, // sinter 0 - 255(center:128)
+  { "temper",    &Temper }, // temper 0 - 255(center:128)
+  { "aecomp",    &AEComp }, // aecomp 0 - 255
+  { "aeitmax",   &AEItMax }, // aeitmax 0-
+  { "dpc",       &DPC }, // dpc 0 - 255
+  { "drc",       &DRC }, // drc 0 - 255
+  { "hilight",   &HiLight }, // hilight 0 - 10
+  { "again",     &AGain }, // again 0 -
+  { "dgain",     &DGain }, // dgain 0 -
+};
+
 char *VideoCapture(int fd, char *tokenPtr) {
 
   char *p = strtok_r(NULL, " \t\r\n", &tokenPtr);
+  if(p) {
+    for(int i = 0; i < sizeof(VideoCommandTable) / sizeof(struct CommandTableSt); i++) {
+      if(!strcmp(p, VideoCommandTable[i].cmd)) return (*VideoCommandTable[i].func)(tokenPtr);
+    }
+  }
+
   int ch = 0;
   if(p && (!strcmp(p, "0") || !strcmp(p, "1"))) {
     ch = atoi(p);
