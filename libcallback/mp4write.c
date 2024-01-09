784d3f1645c11952d7649c8389d624a08a8f73d9
Mitsuru Nakada <mitsuru_nakada@mac.com>
2024-01-10 08:34:33 +0900

    コメント追加

--- libcallback/mp4write.c
+++ libcallback/mp4write.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+/* ===== This file is currently not in use ===== */
+
 static int (*original_mp4write_start_handler)(void *handler, char *file, void *config);
 
 int mp4write_start_handler(void *handler, char *file, void *config) {
